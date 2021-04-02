71ddbea40627b269b8e1bf9762d2bfbe34b506dc
andrea longhi <andrea@spaghetticode.it>
2021-04-02 14:10:04 +0200

    Explicitly require rails-controller-testing
    probably this is necessary with rspec-rails 4.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ ENV['RAILS_ENV'] = 'test'
 # Run Coverage report
 require 'solidus_dev_support/rspec/coverage'
 
+require 'rails-controller-testing'
+Rails::Controller::Testing.install
+
 # Create the dummy app if it's still missing.
 dummy_env = "#{__dir__}/dummy/config/environment.rb"
 system 'bin/rake extension:test_app' unless File.exist? dummy_env
